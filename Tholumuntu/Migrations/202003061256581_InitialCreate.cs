738f6d34884b1aeced70e1ad0d5ebbb45ae87f2b
easysoftware2014 <ayandapatrick@gmail.com>
2020-03-06 22:43:59 +0200

    added UI, User, Profile Controllers and implemented repository pattern

---
+++ Tholumuntu/Migrations/202003061256581_InitialCreate.cs
@@ -0,0 +1,55 @@
+﻿namespace Tholumuntu.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UserProfiles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        LoveLanguage = c.Int(nullable: false),
+                        Horoscope = c.String(),
+                        EntityStatus = c.Int(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                        ModifiedAt = c.DateTime(nullable: false),
+                        User_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.User_Id)
+                .Index(t => t.User_Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Email = c.String(),
+                        Password = c.String(),
+                        ContactNumber = c.String(),
+                        Name = c.String(),
+                        Surname = c.String(),
+                        DateOfBirth = c.DateTime(nullable: false),
+                        Gender = c.String(),
+                        Salt = c.String(),
+                        EntityStatus = c.Int(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                        ModifiedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.UserProfiles", "User_Id", "dbo.Users");
+            DropIndex("dbo.UserProfiles", new[] { "User_Id" });
+            DropTable("dbo.Users");
+            DropTable("dbo.UserProfiles");
+        }
+    }
+}
---
+++ Tholumuntu/Migrations/Configuration.cs
@@ -0,0 +1,58 @@
+﻿using Tholaumuntu.DataAcces.Domain;
+
+namespace Tholumuntu.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TholaUmuntuContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(TholaUmuntuContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            //var user = new User
+            //{
+            //    ContactNumber = "",
+            //    Surname = "Tester",
+            //    CreatedAt = DateTime.Now,
+            //    DateOfBirth = DateTime.Now,
+            //    Email = "tester@gmail.com",
+            //    Gender = "Male",
+            //    ModifiedAt = DateTime.Now,
+            //    Name = "Tester",
+            //    Password = "P@ssw0rd",
+            //    Salt = "HYHT*#%te",
+            //    EntityStatus = EntityStatus.Active,
+            //    Id = 1
+            //};
+
+            //context.Users.AddOrUpdate(user);
+            //context.SaveChanges();
+
+            //var profile = new UserProfile()
+            //{
+            //    CreatedAt = DateTime.Now,
+            //    ModifiedAt = DateTime.Now,
+            //    Horoscope = "GEMINI",
+            //    LoveLanguage = LoveLanguage.PhysicalTouch,
+            //    User = user,
+            //    EntityStatus = EntityStatus.Active,
+            //    Id = 1
+            //};
+
+            //context.UserProfiles.AddOrUpdate(profile);
+            //context.SaveChanges();
+        }
+    }
+}
